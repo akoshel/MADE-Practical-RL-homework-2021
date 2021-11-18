fc21d69c83be1ce87e1f2af09f39e499a0e43eb7
akoshel <johndoe@example.com>
2021-11-18 18:13:05 +0300

    hw3 added

--- hw03_ant/train.py
+++ hw03_ant/train.py
@@ -1,13 +1,14 @@
-import pybullet_envs
-from gym import make
+import copy
+import random
 from collections import deque
+
 import numpy as np
+import pybullet_envs
 import torch
+from gym import make
 from torch import nn
 from torch.nn import functional as F
 from torch.optim import Adam
-import random
-import copy
 
 GAMMA = 0.99
 TAU = 0.002
@@ -18,9 +19,12 @@ BATCH_SIZE = 128
 ENV_NAME = "AntBulletEnv-v0"
 TRANSITIONS = 1000000
 
+
+
 def soft_update(target, source):
     for tp, sp in zip(target.parameters(), source.parameters()):
         tp.data.copy_((1 - TAU) * tp.data + TAU * sp.data)
+
 
 class Actor(nn.Module):
     def __init__(self, state_dim, action_dim):
@@ -33,10 +37,10 @@ class Actor(nn.Module):
             nn.Linear(256, action_dim),
             nn.Tanh()
         )
-        
+
     def forward(self, state):
         return self.model(state)
-        
+
 
 class Critic(nn.Module):
     def __init__(self, state_dim, action_dim):
@@ -48,7 +52,7 @@ class Critic(nn.Module):
             nn.ELU(),
             nn.Linear(256, 1)
         )
-    
+
     def forward(self, state, action):
         return self.model(torch.cat([state, action], dim=-1)).view(-1)
 
@@ -58,34 +62,47 @@ class TD3:
         self.actor = Actor(state_dim, action_dim).to(DEVICE)
         self.critic_1 = Critic(state_dim, action_dim).to(DEVICE)
         self.critic_2 = Critic(state_dim, action_dim).to(DEVICE)
-        
+
         self.actor_optim = Adam(self.actor.parameters(), lr=ACTOR_LR)
         self.critic_1_optim = Adam(self.critic_1.parameters(), lr=ACTOR_LR)
         self.critic_2_optim = Adam(self.critic_2.parameters(), lr=ACTOR_LR)
-        
+
         self.target_actor = copy.deepcopy(self.actor)
         self.target_critic_1 = copy.deepcopy(self.critic_1)
         self.target_critic_2 = copy.deepcopy(self.critic_2)
-        
+
         self.replay_buffer = deque(maxlen=200000)
 
     def update(self, transition):
         self.replay_buffer.append(transition)
         if len(self.replay_buffer) > BATCH_SIZE * 16:
-            
             # Sample batch
-            transitions = [self.replay_buffer[random.randint(0, len(self.replay_buffer)-1)] for _ in range(BATCH_SIZE)]
+            transitions = [self.replay_buffer[random.randint(0, len(self.replay_buffer) - 1)] for _ in
+                           range(BATCH_SIZE)]
             state, action, next_state, reward, done = zip(*transitions)
             state = torch.tensor(np.array(state), device=DEVICE, dtype=torch.float)
             action = torch.tensor(np.array(action), device=DEVICE, dtype=torch.float)
             next_state = torch.tensor(np.array(next_state), device=DEVICE, dtype=torch.float)
             reward = torch.tensor(np.array(reward), device=DEVICE, dtype=torch.float)
             done = torch.tensor(np.array(done), device=DEVICE, dtype=torch.float)
-            
+            with torch.no_grad():
+                target_action = self.target_actor(next_state)
+                q_target = reward + GAMMA * (1 - done) * torch.min(
+                    self.target_critics_1(next_state, target_action),
+                    self.target_critics_2(next_state, target_action)
+                )
             # Update critic
-            
+            loss1 = F.mse_loss(self.critic_1(state, action), q_target)
+            loss2 = F.mse_loss(self.critic_2(state, action), q_target)
+
+            self.critic_1_optim.zero_grad()
+            loss1.backward()
+            self.critic_1_optim.step()
+            self.critic_2_optim.zero_grad()
+            loss2.backward()
+            self.critic_2_optim.step()
             # Update actor
-            
+
             soft_update(self.target_critic_1, self.critic_1)
             soft_update(self.target_critic_2, self.critic_2)
             soft_update(self.target_actor, self.actor)
@@ -105,12 +122,13 @@ def evaluate_policy(env, agent, episodes=5):
         done = False
         state = env.reset()
         total_reward = 0.
-        
+
         while not done:
             state, reward, done, _ = env.step(agent.act(state))
             total_reward += reward
         returns.append(total_reward)
     return returns
+
 
 if __name__ == "__main__":
     env = make(ENV_NAME)
@@ -120,20 +138,20 @@ if __name__ == "__main__":
     episodes_sampled = 0
     steps_sampled = 0
     eps = 0.2
-    
+
     for i in range(TRANSITIONS):
         steps = 0
-        
-        #Epsilon-greedy policy
+
+        # Epsilon-greedy policy
         action = td3.act(state)
         action = np.clip(action + eps * np.random.randn(*action.shape), -1, +1)
 
         next_state, reward, done, _ = env.step(action)
         td3.update((state, action, next_state, reward, done))
-        
+
         state = next_state if not done else env.reset()
-        
-        if (i + 1) % (TRANSITIONS//100) == 0:
+
+        if (i + 1) % (TRANSITIONS // 100) == 0:
             rewards = evaluate_policy(test_env, td3, 5)
-            print(f"Step: {i+1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}")
+            print(f"Step: {i + 1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}")
             td3.save()
